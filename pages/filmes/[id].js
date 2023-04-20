cac41198fde6966b0ed2f73bb6359caaa259414f
pedroblima <pedrob@iesb.edu.br>
2023-04-20 20:22:17 -0300

    lista id

--- pages/filmes/[id].js
+++ pages/filmes/[id].js
@@ -1,14 +1,38 @@
 import Pagina from '@/components/Pagina'
 import apiFilmes from '@/services/apiFilmes'
 import React from 'react'
+import { Card, Col, Row } from 'react-bootstrap'
 
-const Detalhes = ({filme}) => {
+const Detalhes = ({ filme }) => {
   return (
     <Pagina titulo={filme.title}>
-        <div></div>
+      <Row>
+        <Col md={3}>
+          <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + filme.poster_path} />
+        </Col>
+        <Col md={9}>
+          <p><strong>Orçamentos: </strong>{filme.budget}</p>
+          <p><strong>Data de Lançamentos: </strong>{filme.release_date}</p>
+          <p><strong>Duração: </strong>{filme.runtime} min</p>
+          <p><strong>Nota: </strong>{filme.vote_average}</p>
+
+
+          <div> 
+            <strong>Generos:</strong>
+            <ul>
+              {filme.genres.map(item => (
+                <li>{item.name}</li>
+              ))}
+            </ul>
+          </div>
+          
+          <p>{filme.overview}</p>
+
+        </Col>
+      </Row>
 
     </Pagina>
-  
+
   )
 }
 
@@ -16,12 +40,12 @@ export default Detalhes
 
 export async function getServerSideProps(context) {
 
-    const id = context.params.id
+  const id = context.params.id
 
-    const resultado = await apiFilmes.get('/movie/' + id)
-    const filme = resultado.data
+  const resultado = await apiFilmes.get('/movie/' + id)
+  const filme = resultado.data
 
-    return {
-        props: { filme },
-    }
+  return {
+    props: { filme },
+  }
 }
